5a39ba4083b542e698c68b9abd3d2fa8896b0bff
Asia S <89806443+shellam4@users.noreply.github.com>
2021-09-04 16:59:34 -0700

    Created mario2 (more comfortable)

---
+++ Week 1/ProblemSets/mario2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main() {
+    // prompt the user for a positive int for the height
+    int height;
+    do {
+        height = get_int("Height: ");
+    }
+    while(height % 2 != 0 || height > 8 || height < 1);
+    
+    // print the pyramid
+    for (int row = 1; row <= height; row++) {
+       // print the spaces to make it right aligned
+       for (int k = 1; k <= height - row; k++) {
+            printf(" ");
+
+       }
+       // print row the correct number of times
+        for(int j = 1; j <= row; j++) {
+            printf("#");
+        }
+        // print the spaces in between
+        printf("  ");
+        
+        // print the other rows the correct number of times
+        for(int g = 1; g <= row; g++) {
+            printf("#");
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
